fcef5cece872c43e413f9d383212f285c4caec87
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-09 14:07:36 -0800

    add exercise2

--- functions.js
+++ functions.js
@@ -8,7 +8,10 @@ printSimpleGreeting ();
 
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
-
+function printGreeting(name) {
+    console.log(`Hola, ${name}!`);
+}
+printGreeting ("Aladdin"); 
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
