9123f5f01710bb38cdcaf2bda2ec85daf912fda6
Caitlin Cradick <caitlincradick@gmail.com>
2022-12-09 13:41:25 -0800

    add ex 1

--- cleanUp.js
+++ cleanUp.js
@@ -9,38 +9,39 @@
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+nameQuestion();
+// // I added semicolons to the end of the console log and calling the condition to follow the convention/best practice
+// // everything else looks okay
+
+// // EX 2:
+// function addThreeNums(first, second, third) {
+// var sum = first + second + third
+// console.log(sum);
+//       }
+
+// addThreeNums(1, 2, 3);
+// addThreeNums(4, 2, 7);
 
 
-// EX 2:
-function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+// // EX 3:
+// func makeFreshPesto(){
+//   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
+//   console.log("Pulse basil and pine nuts");
+//   console.log("Add garlic and cheeses");
+//   console.log("Slowly pour in oil");
+//   console.log("Season");    }
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
-
-
-// EX 3:
-func makeFreshPesto(){
-  console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
-  console.log("Pulse basil and pine nuts");
-  console.log("Add garlic and cheeses");
-  console.log("Slowly pour in oil");
-  console.log("Season");    }
-
-makeFreshPesto();
+// makeFreshPesto();
 
 
-//  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+// //  EX 4:
+// function average(num1, num2) 
+//   {
+// var sum = num1 + num2;
+//     var avg = sum / 2;
 
-  console.log(`the average is: ${avg}`.)
-  }
+//   console.log(`the average is: ${avg}`.)
+//   }
